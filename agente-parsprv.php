bab486e48f970ba0aa86b2f54fa6d2402222fc96
Marco Baranello <baranellom@gmail.com>
2019-11-04 11:04:29 -0300

    Add files via upload

---
+++ agente-parsprv.php
@@ -0,0 +1,43 @@
+#!/usr/bin/php
+
+<?php
+        echo "F/H Inicio: " . date('d/m/Y H:i:s');
+
+        //Creo la variable con la consulta a la BD Local
+        //$consultalocal="CALL mrp_parsprv;";
+        $consultalocal="CALL mrp_parsprv;";
+
+        //Creas una variable de tipo objeto mysqli con los datos de la bd y el charset que quieras
+        $mysql_loc = mysqli_connect('192.168.0.157', 'root','','fc');
+        //var_dump($mysql_loc);
+
+        //Chequeo que la conexion Local se haya establecido bien
+        if(!$mysql_loc)
+        {
+                echo "No se ha podido establecer la conexion Local PHP - MySQL, verifique sus datos Locales.\r".PHP_EOL ;
+                echo mysqli_connect_errno().PHP_EOL;
+                echo mysqli_connect_error().PHP_EOL;
+                exit();
+        }
+        else /*conexion local exitosa*/
+        {
+                $mysql_loc->set_charset("utf8");
+
+                //Realizo la consulta a la Base de Datos Local
+                if (mysqli_query($mysql_loc, $consultalocal) === TRUE)
+                {
+                    mysqli_close($mysql_loc);
+                    echo " Finalizado con exito.-";
+                    echo " F/H Final: " . date('d/m/Y H:i:s');
+                    exit();
+                }
+                else //Si la consulta local no devuelve resultados
+                {
+                        mysqli_close($mysql_loc);
+                        echo " Conexion Existosa, pero no se ejecuto bien el SP.- ";
+                        echo " F/H Final: " . date('d/m/Y H:i:s');
+                        exit();
+                }
+        }
+
+?>
