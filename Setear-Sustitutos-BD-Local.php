da6b5389a659320f2dc8b379af4bdec9a017c8fa
Marco Baranello <baranellom@gmail.com>
2020-03-20 12:31:06 -0300

    nuevo archivo

--- Setear-Sustitutos-BD-Local.php
+++ Setear-Sustitutos-BD-Local.php
@@ -32,7 +32,7 @@ $Articulos_magento = mysqli_query ( $enlace, $Consulta_inicial );
 #-- Con los productos obtenidos chequeo existencia del Archivo en Directorio
 while ($art_magento = mysqli_fetch_array($Articulos_magento)):
 
-	$Consulta_Sustitutos = "SELECT r_prd_prd.prd_id1 as id, GROUP_CONCAT(r_prd_prd.prd_id2) as sustitutos from r_prd_prd WHERE r_prd_prd.prd_id1 = ".$art_magento['id_gestuc']." group by r_prd_prd.prd_id1 order by prd_id2 ;";
+	$Consulta_Sustitutos = "SELECT GROUP_CONCAT(r_prd_prd.prd_id2) as sustitutos from r_prd_prd WHERE r_prd_prd.prd_id1 = ".$art_magento['id_gestuc']." group by r_prd_prd.prd_id1 order by prd_id2 ;";
     $Sustitutos = mysqli_query($enlace, $Consulta_Sustitutos);
 
     while ($Art_Sustitutos = mysqli_fetch_array($Sustitutos)):
@@ -42,22 +42,11 @@ while ($art_magento = mysqli_fetch_array($Articulos_magento)):
 		#-- Chequeo que el archivo con el nombre de la imagen exista
 		echo "Existen productos relacionados \n";
 
-		$Modificar_registro = "UPDATE articulos_magento SET related_skus='".$Art_Sustitutos['sustitutos']."' WHERE  sku='".$art_magento['sku']."'
+		$Modificar_registro = "UPDATE articulos_magento SET related_skus='".$Art_Sustitutos['sustitutos']."' WHERE  sku='".$art_magento['sku']."' LIMIT 1;";
         
-        UPDATE articulos_magento SET existeimagen='1', fecha_up = now() WHERE sku='".$art_magento['sku']."' LIMIT 1;";
-
-		$Mod_reg = mysqli_query($enlace, $Modificar_registro);
-		 
-		else 
-		{
-			echo "El fichero $nombre_fichero no existe \n";
-			
-			$Modificar_registro = "UPDATE articulos_magento SET existeimagen='0' WHERE sku='".$art_magento['sku']."' LIMIT 1;";
-
-			$Mod_reg = mysqli_query($enlace, $Modificar_registro);
-		}
+        $Mod_reg = mysqli_query($enlace, $Modificar_registro);
 	}
-	
+
 
 endwhile;
 
