fd9b668d0702da9868cbd96eba0e62eb2995be94
Marco Baranello <baranellom@gmail.com>
2020-03-25 13:07:04 -0300

    ultimos cambios

--- Setear-Sustitutos-BD-Local.php
+++ Setear-Sustitutos-BD-Local.php
@@ -32,7 +32,7 @@ $Articulos_magento = mysqli_query ( $enlace, $Consulta_inicial );
 #-- Con los productos obtenidos chequeo existencia del Archivo en Directorio
 while ($art_magento = mysqli_fetch_array($Articulos_magento)):
 
-	$Consulta_Sustitutos = "SELECT GROUP_CONCAT(r_prd_prd.prd_id2) as sustitutos from r_prd_prd WHERE r_prd_prd.prd_id1 = ".$art_magento['id_gestuc']." group by r_prd_prd.prd_id1 order by prd_id2 ;";
+	$Consulta_Sustitutos = "SELECT GROUP_CONCAT( CONCAT(r_prd_prd.prd_id2,'-1') ) as sustitutos from r_prd_prd WHERE r_prd_prd.prd_id1 = ".$art_magento['id_gestuc']." group by r_prd_prd.prd_id1 order by prd_id2 ;";
     $Sustitutos = mysqli_query($enlace, $Consulta_Sustitutos);
 
     while ($Art_Sustitutos = mysqli_fetch_array($Sustitutos)):
