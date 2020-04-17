7fe1d51299d71216e074c76166b10164cc6f71f4
Marco Baranello <baranellom@gmail.com>
2020-04-17 08:22:58 -0300

    confirmado

--- agente-actualizacion-masiva-precios.php
+++ agente-actualizacion-masiva-precios.php
@@ -2,28 +2,16 @@
 
 <?php
 
-// var_dump($argv);
-// array(4) {
-// 	[0]=>
-// 	string(10) "script.php"
-// 	[1]=>
-// 	string(4) "arg1"
-// 	[2]=>
-// 	string(4) "arg2"
-// 	[3]=>
-// 	string(4) "arg3"
-//   }
-
 //$DIRHOME="/usr/share/Alertas/";
 $DIRHOME = "D:/ProyectosVariosMipol/Agentes-Mipol/";
-$ARCHIVOSQL = "Sql_Update.sql";
+$ARCHIVOSQL = "Actualizacion_Precios.sql";
 $MAILSISTEMA = "sistema@mipolrepuestos.com";
 $MAILTEST = "mbaranello@mipolrepuestos.com";
 $MAILSAMMY = "sammy.moreno@microsolutions.cl";
 
 if ($argv[1] == "")
 {
-	$Coeficiente = '1.08';
+	$Coeficiente = '1.07';
 	echo "No tiene argumentos. Coeficiente de aumento : ".$Coeficiente."\n";
 }
 else
@@ -52,22 +40,22 @@ if (mysqli_connect_errno ()) {
 	exit ();
 }
 
-$Lineas_Sql= fopen('Sql_Update.sql',"w");
+#-- Abro archivo para agregar lineas sql con actualizacion de precios
+$Lineas_Sql= fopen('Sql_precios_Update.sql',"w");
 
 //$Consulta_Productos= "select distinct prd.prd_id from prd inner join preciovta on prd.prd_id = preciovta.prd_id WHERE prd.prd_id <> 0 order by prd.prd_id ;";
 
 //$Consulta_Productos= "select distinct prd.prd_id from prd inner join preciovta on prd.prd_id = preciovta.prd_id WHERE prd.prd_id <> 0 and prd.marcaproducto_id IN (84, 106, 28, 29, 16, 531, 510, 120, 100055, 100341) ORDER by prd.prd_id ;";
 
-$Consulta_Productos= "select distinct prd.prd_id from prd inner join preciovta on prd.prd_id = preciovta.prd_id WHERE prd.prd_id <> 0 and prd.marcaproducto_id IN (130) ORDER by prd.prd_id ;";
+$Consulta_Productos= "select distinct prd.prd_id from prd inner join preciovta on prd.prd_id = preciovta.prd_id WHERE prd.prd_id <> 0 and prd.marcaproducto_id NOT IN (49, 100156) ORDER by prd.prd_id ;";
 
 $Productos = mysqli_query ( $enlace, $Consulta_Productos );
 
+#-- Consulta para obtener en Maximo id de la tabla de precios
+$Consulta_max_id = "SELECT MAX(preciovta_id) as id FROM preciovta;";
 
-//Obtengo en Maximo id de la tabla de precios
-$ConsultaSQL2 = "SELECT MAX(preciovta_id) as id FROM preciovta;";
-
-//Ahora obtengo el Maximo Id de la Tabla preciovta para incrementarlo en 1 y grabar nuevo registro
-$Id_Maximo = mysqli_query ( $enlace, $ConsultaSQL2 );
+#-- Ahora obtengo el Maximo Id de la Tabla preciovta para incrementarlo en 1 y grabar nuevo registro
+$Id_Maximo = mysqli_query ( $enlace, $Consulta_max_id );
 $max_id = mysqli_fetch_array($Id_Maximo);
 $id_siguiente = $max_id['id'];
 
@@ -93,35 +81,30 @@ while ($prd = mysqli_fetch_array($Productos)) {
 	where prd_id=".$prd['prd_id']." 
 	order by preciovta_id desc LIMIT 1;";
 
-	//Obtengo en Maximo id de la tabla de precios
-	//$ConsultaSQL2 = "SELECT MAX(preciovta_id) as id FROM preciovta;";
-
 	if ($resultado = mysqli_query ( $enlace, $ConsultaSQL )) {
 		
 		$id_siguiente = $id_siguiente + 1;
 
 		$preciovta = mysqli_fetch_array($resultado);
 		
-		//Ahora obtengo el Maximo Id de la Tabla preciovta para incrementarlo en 1 y grabar nuevo registro
-		// $Id_Maximo = mysqli_query ( $enlace, $ConsultaSQL2 );
-		// $max_id = mysqli_fetch_array($Id_Maximo);
-		// $id_siguiente = $max_id['id'] + 1;
-
 		$Consulta_agregar = "Insert into preciovta (preciovta_id,preciovta_precio,prd_id,preciovta_vigencia,preciovta_mayorista,preciovta_minorista,preciovta_asociados,costo,condicionesdescuentos,preciovta_usuario) 
 		values (".$id_siguiente.",". number_format( $preciovta['preciovta_precio'] * $Coeficiente,4,'.','') .",". $preciovta['prd_id'] .",\"". date('Y-m-d') ."\",". $preciovta['preciovta_mayorista'] .",". $preciovta['preciovta_minorista'] .",". $preciovta['preciovta_asociados'] .",". number_format($preciovta['costo'] * $Coeficiente,4,'.','') .",\"". $preciovta['condicionesdescuentos'] ."\",". $preciovta['preciovta_usuario'] .")";
 
-		$Insertar_tabla = mysqli_query ($enlace, $Consulta_agregar);
+		//$Consulta_agregar = "Insert into preciovta (preciovta_id,preciovta_precio,prd_id,preciovta_vigencia,preciovta_mayorista,preciovta_minorista,preciovta_asociados,costo,condicionesdescuentos,preciovta_usuario) 
+		//values (".$id_siguiente.",". number_format( $preciovta['preciovta_precio'] * $Coeficiente,4,'.','') .",". $preciovta['prd_id'] .",'". date('Y-m-d') ."',". $preciovta['preciovta_mayorista'] .",". $preciovta['preciovta_minorista'] .",". $preciovta['preciovta_asociados'] .",". number_format($preciovta['costo'] * $Coeficiente,4,'.','') .",'". $preciovta['condicionesdescuentos'] ."',". $preciovta['preciovta_usuario'] .")";
 
-		$Consulta_agregar_log = "INSERT INTO log (sentencia,fecha,tabla,usuario_id,suc) values ('".$Consulta_agregar."',\"". date('Y-m-d') ."\",\"preciovta\",158,9);";
+		#$Insertar_tabla = mysqli_query ($enlace, $Consulta_agregar);
 
-		$Insertar_log = mysqli_query ($enlace, $Consulta_agregar_log);
+		//$Consulta_agregar_log = "INSERT INTO log (sentencia,fecha,tabla,usuario_id,suc) values ('".$Consulta_agregar."',\"". date('Y-m-d') ."\",\"preciovta\",158,9);";
+		//$Consulta_agregar_log = "INSERT INTO log (sentencia,fecha,tabla,usuario_id,suc) values (\"".$Consulta_agregar."\",\"". date('Y-m-d') ."\",\"preciovta\",1,9);";
+
+		#$Insertar_log = mysqli_query ($enlace, $Consulta_agregar_log);
 
 		// echo $Consulta_agregar . ";\n";
 		// echo $Consulta_agregar_log . "\n";
 		
-		//$linea7 = $art_hab['prd_id'].','.$art_hab['prd_codalfa'].',"'.$art_hab['Division'].'",'.$art_hab['Clasificacion'].',"'.$art_hab['Detalle'].'",'.$SxS[$a-1].",\"".$fuv['fuv']."\"\r\n";
 		fwrite($Lineas_Sql,$Consulta_agregar.";". "\r\n");
-		fwrite($Lineas_Sql,$Consulta_agregar_log . "\r\n");
+		//fwrite($Lineas_Sql,$Consulta_agregar_log . "\r\n");
 
 		/* liberar el conjunto de resultados */
 		mysqli_free_result ( $resultado );
@@ -175,7 +158,7 @@ fclose($Lineas_Sql);
 
 // $mail->Send ();
 
-echo "Informes enviados." . date('r'). "\n";
+echo "Proceso Terminado." . date('r'). "\n";
 
 // Borro los Archivos Generados
 //unlink ( $DIRHOME . $ARCHIVOSQL );
